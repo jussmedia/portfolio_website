dbbbcf4d69e9106a7ac295aeb6cda7a1654d9582
Julian Skinner <j.skinner@arpc.com>
2014-11-13 09:27:09 -0500

    Implementation for about layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( *.js *.png *.gif *.eot *.woff *.ttf *.svg)
+Rails.application.config.assets.precompile += %w( *.js *.css *.png *.gif *.eot *.woff *.ttf *.svg)
