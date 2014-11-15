a1c1e73d5c1405f26366109bc107f57fe49d670a
Julian Skinner <j.skinner@arpc.com>
2014-11-15 17:29:00 -0500

    direct all provider callbacks to the 'Authentications' <- yet to be determined controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'about/index'
 
+  # OmniAuthentications will be handled by the AuthenticationsController
+  # get "/auth/:provider/callback" => "authentications#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
