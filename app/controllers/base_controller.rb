0b28cadb2195c9e20fb0c83ea36c5a5b9505c003
Julian Skinner <j.skinner@arpc.com>
2014-11-17 09:36:56 -0500

    allows to track a guest user and transfer over to a registered user in the db.

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -13,4 +13,50 @@ class BaseController < ActionController::Base
   end
   helper_method :tag_line
 
+  # If the user logged in, return current otherwise return guest
+  def current_or_guest_user
+    if current_user
+      if session[:guest_user_id] && session[:guess_user_id] != current_user
+        logging_in
+        guest_user(with_retry = false).try(:destroy)
+        session[:guest_user_id] = nil
+      end
+      current_user
+    else
+      guest_user
+    end
+  end
+  helper_method :current_or_guest_user
+  
+  # find the guest_user object associated with the current session or
+  # create one if not found
+  def guest_user(with_retry = true)
+    @cached_guest_user ||= User.find(session[:guest_user_id] ||= create_guest_user.id)
+
+  rescue ActiveRecord::RecordNotFound
+    session[:guest_user_id] = nil
+    guest_user if with_retry
+  end
+
+  private
+
+    # Purpose of this will be to hand off any thing the guest user
+    # did to a real user.
+    def logging_in
+      # At some point this will need to occur
+      # For example:
+        # guest_comments = guest_user.comments.all
+        # guest_comments.each do |comment|
+          # comment.user_id = current_user.id
+          # comment.save!
+        # end
+    end
+
+    def create_guest_user
+      u = User.create(first_name: 'guest', last_name: 'guest',
+        e_mail: "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+      u.save!(validate: false)
+      session[:guest_user_id] = u.id
+      u
+    end
 end
