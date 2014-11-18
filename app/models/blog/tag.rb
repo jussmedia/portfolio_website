0c15f5f1a282e0eb4dddb04cf32996b2019df9b6
Julian Skinner <j.skinner@arpc.com>
2014-11-18 18:37:08 -0500

    added uniqueness validation.

--- app/models/blog/tag.rb
+++ app/models/blog/tag.rb
@@ -2,8 +2,8 @@ class Blog::Tag < ActiveRecord::Base
   has_many :taggings
   has_many :posts, through: :taggings
 
-  validates :name, presence: true
-  
+  validates :name, presence: true, uniqueness: true
+
   def to_s
     name
   end
--- test/models/blog/tag_test.rb
+++ test/models/blog/tag_test.rb
@@ -10,6 +10,14 @@ describe Blog::Tag do
     t.errors.messages[:name].must_be :present?
   end
 
+  it 'must be unique' do
+    t = Blog::Tag.create(name: 'technology')
+    t.valid?
+
+    t.errors.messages[:name].must_be :present?
+    t.errors.messages[:name].first.must_equal 'has already been taken'
+  end
+  
   it "must be valid" do
     tags.must_be :valid?
   end
