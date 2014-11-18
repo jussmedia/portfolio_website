7722144d1f83dd1b52ed1af8c8a9eeb60206ef44
Julian Skinner <j.skinner@arpc.com>
2014-11-18 17:43:14 -0500

    Updated samples to ensure pagination works as expected and also randomized dates.

--- db/samples.rb
+++ db/samples.rb
@@ -6,13 +6,14 @@
 #
 #******************
 puts "Creating sample blog posts..."
-post_count = 10
+post_count = 33
 p = ProgressBar.create(title: "Sample Posts", total: post_count)
 post_count.times do
   post = Blog::Post.new(title: Faker::Lorem.sentence,
-                 body: Faker::Lorem.paragraph,
+                 body: Faker::Lorem.paragraph([2...8].sample),
                  author: Faker::Name.name,
-                 published: [true, false].sample)
+                 published: [true, false].sample,
+                 created_at: Faker::Time.between(5.months.ago, Time.now))
 
   post.save
   p.increment
